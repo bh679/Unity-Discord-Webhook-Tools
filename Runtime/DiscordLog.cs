ccb1e734f1d8729e44b115ab0a142142847a3af7
Brennan Hatton <brennan@brennanhatton.com>
2023-03-01 13:58:25 +1100

    MonoFloats

---
+++ Runtime/DiscordLog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrennanHatton.Discord
+{
+    public class DiscordLog : MonoBehaviour
+	{
+		public string preMessage, postMessage;
+		
+		public void ClassStarted(string _message)
+		{
+			DiscordLogManager.Instance.SendMessage(preMessage + _message + postMessage);
+		}
+    }
+}
